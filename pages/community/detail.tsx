70f9fe628c54f5032acab46e367abc37d4cc34ae
gayratjon-02 <s-202386005@sillain.ac.kr>
2026-04-25 11:22:36 +0900

    fix(community-detail): wrong like mutation was 400ing
    detail page imported LIKE_TARGET_BOARD_ARTICLE which calls a
    'likeTargetArticle' resolver the backend doesn't expose, while the
    list page uses LIKE_TARGET_BOARD_ARTICLE_BY_BOARD_NAME which calls
    the actual 'likeTargetBoardArticle' resolver. switch detail to the
    same mutation so the like button works.

--- pages/community/detail.tsx
+++ pages/community/detail.tsx
@@ -21,7 +21,7 @@ import { T } from '../../libs/types/common';
 import EditIcon from '@mui/icons-material/Edit';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { BoardArticle } from '../../libs/types/board-article/board-article';
-import { CREATE_COMMENT, LIKE_TARGET_BOARD_ARTICLE, UPDATE_COMMENT } from '../../apollo/user/mutation';
+import { CREATE_COMMENT, LIKE_TARGET_BOARD_ARTICLE_BY_BOARD_NAME, UPDATE_COMMENT } from '../../apollo/user/mutation';
 import { GET_BOARD_ARTICLE, GET_COMMENTS } from '../../apollo/user/query';
 import { Messages } from '../../libs/config';
 import {
@@ -69,7 +69,7 @@ const CommunityDetail: NextPage = ({ initialInput, ...props }: T) => {
 	const [boardArticle, setBoardArticle] = useState<BoardArticle>();
 
 	/** APOLLO REQUESTS **/
-	const [likeTargetBoardArticle] = useMutation(LIKE_TARGET_BOARD_ARTICLE);
+	const [likeTargetBoardArticle] = useMutation(LIKE_TARGET_BOARD_ARTICLE_BY_BOARD_NAME);
 	const [createComment] = useMutation(CREATE_COMMENT);
 	const [updateComment] = useMutation(UPDATE_COMMENT);
 
